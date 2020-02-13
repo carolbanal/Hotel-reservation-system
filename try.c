4408f2ad6ce01e6972b4d4dc4e6b8fca19fe9941
carolbanal <carolbanal@gmail.com>
2020-02-13 10:54:56 +0800

    fixing check out date

--- try.c
+++ try.c
@@ -7,6 +7,8 @@
 #include<dos.h>
 #include<time.h>
 
+int valid_date(int date, int mon, int year);
+
 void book();
 void view();
 void customer();
@@ -77,10 +79,15 @@ void book(){
 	
 	FILE *fptr;
 	FILE *fp;
-	int indate, outdate, days, noAmenities, a[5], totalAmount;
+	int indate, outdate, noAmenities, a[5], totalAmount;
 	char rchoice;
 	int single=1500, doubl=2500, deluxe=3500, amount;
 	int i=0, j=0;
+	int day1, mon1, year1,
+        day2, mon2, year2;
+ 
+    int days, mon_diff, year_diff;  
+	
 	
 	fp =fopen("Records.txt", "r+");
 	fptr = fopen("Receipt.txt", "w+");
@@ -97,13 +104,69 @@ void book(){
 	printf("\t\t\t\t                      Enter Details                     \n");
 	printf("\t\t\t\t                 -----------------------                \n");
 	printf("\t\t\t\t                                                        \n");
-	printf("\t\t\t\t                 Check-in date(dd): ");
-	scanf ("%i",&indate);
-	printf("\t\t\t\t                 Check-out date(dd): ");
-	scanf ("%i",&outdate);
+	printf("\t\t\t\t                 Enter Check in Date (MM/DD/YYYY): ");
+    scanf("%d/%d/%d", &mon1, &day1, &year1);
+    
+    printf("\t\t\t\t                 Enter out date (MM/DD/YYYY): ");
+    scanf("%d/%d/%d", &mon2, &day2, &year2);
 	
-	days = outdate-indate;//formula for days
-	printf("\t\t\t\t                 Days: %i\n", days);
+		if(!valid_date(day1, mon1, year1))
+	    {
+	        printf("\t\t\t\t                 First date is invalid.\n");        
+	    }
+	    
+	    if(!valid_date(day2, mon2, year2))
+	    {
+	        printf("\t\t\t\t                 Second date is invalid.\n");
+	        exit(0);
+	    }       
+	    
+	    if(day2 < day1)
+	    {      
+	        // borrow days from february
+	        if (mon2 == 3)
+	        {
+	            //  check whether year is a leap year
+	            if ((year2 % 4 == 0 && year2 % 100 != 0) || (year2 % 400 == 0)) 
+	            {
+	                day2 += 29;
+	            }
+	            
+	            else
+	            {
+	                day2 += 28;
+	            }                        
+	        }
+	        
+	        // borrow days from April or June or September or November
+	        else if (mon2 == 5 || mon2 == 7 || mon2 == 10 || mon2 == 12) 
+	        {
+	           day2 += 30; 
+	        }
+	                
+	        // borrow days from Jan or Mar or May or July or Aug or Oct or Dec
+	        else
+	        {
+	           day2 += 31;
+	        }
+	        
+	        mon2 = mon2 - 1;
+	    }
+	    
+	    if (mon2 < mon1)
+	    {
+	        mon2 += 12;
+	        year2 -= 1;
+	    }       
+	    
+	    days = day2 - day1;
+	    mon_diff = mon2 - mon1;
+	    year_diff = year2 - year1;
+	    
+	    printf("\t\t\t\t                 You will be staying for: %2d days\n", days);
+	    
+	    
+	
 	printf("\t\t\t\t                 Type of room: [1] Single Room\n");
 	printf("\t\t\t\t                               [2] Double Room\n");
 	printf("\t\t\t\t                               [3] Deluxe\n");
@@ -272,4 +335,65 @@ SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
 	
 	
 	
+}
+
+int valid_date(int day, int mon, int year)    
+{
+    int is_valid = 1, is_leap = 0;
+    
+    if (year >= 1800 && year <= 9999) 
+    {
+ 
+        //  check whether year is a leap year
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
+        {
+            is_leap = 1;
+        }
+        
+        // check whether mon is between 1 and 12
+        if(mon >= 1 && mon <= 12)
+        {
+            // check for days in feb
+            if (mon == 2)
+            {
+                if (is_leap && day == 29) 
+                {
+                    is_valid = 1;
+                }
+                else if(day > 28) 
+                {
+                    is_valid = 0;
+                }
+            }
+            
+            // check for days in April, June, September and November
+            else if (mon == 4 || mon == 6 || mon == 9 || mon == 11) 
+            {
+                if (day > 30)
+                {
+                    is_valid = 0;
+                }
+            }
+ 
+            // check for days in rest of the months 
+            // i.e Jan, Mar, May, July, Aug, Oct, Dec
+            else if(day > 31)
+            {            
+                is_valid = 0;
+            }        
+        }
+        
+        else
+        {
+            is_valid = 0;
+        }
+ 
+    }
+    else
+    {
+        is_valid = 0;
+    }
+    
+    return is_valid;
+        
 }
